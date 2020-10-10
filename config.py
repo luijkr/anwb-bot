af15e6b85a95ede3c755c844a327da14a1f3661b
René <luijk.r@gmail.com>
2020-10-10 09:29:48 +0200

    Format response, add messages/image

--- config.py
+++ config.py
@@ -1,3 +1,71 @@
+class Parameter:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+    def to_dict(self):
+        return {self.key: self.value}
+
+
 class Config:
     TOKEN = "1318733250:AAEJXft0OfQ-cVsoucGgyeKeBdRQunRQEGY"
     CHAT_ID = "1325598558"
+    HOURS = 12
+
+    # API endoint
+    ENDPOINT = "https://api.anwb.nl/occasion-hexon-search"
+
+    # URL query parameters
+    BRAND = Parameter("vehicle.brand.keyword", "renault")
+    MODEL = Parameter("vehicle.model.keyword", "clio")
+    COORDINATES = Parameter("company.coordinates", "2171xv-100km")
+    PRICE = Parameter("vehicle.askingPrice", "0-9500")
+    FROM = Parameter("from", "2014")
+    MILEAGE = Parameter("vehicle.mileageInKm", "0-90000")
+    BODYSHAPE = Parameter("vehicle.bodyShape.keyword", "HATCHBACK")
+    DOORS = Parameter("vehicle.numberOfDoors", "4-5")
+    LIMIT = Parameter("limit", "100")
+    VIEW = Parameter("viewwrapper", "grid")
+
+    # headers
+    CONNECTION = Parameter("Connection", "keep-alive")
+    ACCEPT = Parameter("Accept", "application/json")
+    DNT = Parameter("DNT", "1")
+    CLIENT_ID = Parameter("x-anwb-client-id", "innzlrw2VjJNTsfWGaTK0C887VOO5mIJ")
+    USER_AGENT = Parameter("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36")
+    ORIGIN = Parameter("Origin", "https://www.anwb.nl")
+    FETCH_SITE = Parameter("Sec-Fetch-Site", "same-site")
+    FETCH_MODE = Parameter("Sec-Fetch-Mode", "cors")
+    FETCH_DEST = Parameter("Sec-Fetch-Dest", "empty")
+    REF = Parameter("Referer", "https://www.anwb.nl/")
+    ACCEPT_LANGUAGE = Parameter("Accept-Language", "en-US,en;q=0.9,hr-HR;q=0.8,hr;q=0.7,nl;q=0.6,la;q=0.5")
+    COOKIE = Parameter("Cookie", "optimizelyEndUserId=oeu1602049939765r0.9981325892211941; _cs_mk=0.49465314211777023_1603; _gid=GA1.2.1331447756.1602161480; SSO=TkT0b8SnLF3AddQ; AccessToken=w9c64b5635bxzndk; login-expires=1602170756513; login-name=R.%20Luijk; _ga_9ZJ9JFZNEG=GS1.1.1602169460.5.1.1602169488.0; _ga=GA1.2.1172286515.1602049940")
+
+    def get_url_params(self):
+        return {
+            **self.BRAND.to_dict(),
+            **self.MODEL.to_dict(),
+            **self.COORDINATES.to_dict(),
+            **self.PRICE.to_dict(),
+            **self.FROM.to_dict(),
+            **self.MILEAGE.to_dict(),
+            **self.BODYSHAPE.to_dict(),
+            **self.DOORS.to_dict(),
+            **self.LIMIT.to_dict(),
+            **self.VIEW.to_dict()
+        }
+
+    def get_headers(self):
+        return {
+            **self.CONNECTION.to_dict(),
+            **self.ACCEPT.to_dict(),
+            **self.DNT.to_dict(),
+            **self.CLIENT_ID.to_dict(),
+            **self.USER_AGENT.to_dict(),
+            **self.ORIGIN.to_dict(),
+            **self.FETCH_SITE.to_dict(),
+            **self.FETCH_MODE.to_dict(),
+            **self.FETCH_DEST.to_dict(),
+            **self.REF.to_dict(),
+            **self.ACCEPT_LANGUAGE.to_dict(),
+            **self.COOKIE.to_dict()
+        }
--- main.py
+++ main.py
@@ -1,15 +1,92 @@
+import requests
+from datetime import datetime, timedelta
+from telegram.ext import Updater
 from config import Config
-from telegram.ext import Updater, CommandHandler
 
-conf = Config
+conf = Config()
+
+
+def call_api():
+    # import json
+    # return json.load(open("response.json"))
+    params = conf.get_url_params()
+    headers = conf.get_headers()
+    r = requests.get(conf.ENDPOINT, params=params, headers=headers)
+    return r.json()
+
+
+def get_latest(results):
+    ts = datetime.now() - timedelta(hours=conf.HOURS)
+    filtered = [
+        doc for doc in results
+        if datetime.fromtimestamp(doc.get("meta").get("createdAt") / 1000.0) > ts
+    ]
+    return sorted(filtered, key=lambda k: k.get("meta").get("createdAt"), reverse=True)
+
+
+def get_cover_image(doc):
+    media = [
+        img for img in doc.get("vehicle").get("media")
+        if "https" in img.get("value")
+    ]
+    if len(media) > 0:
+        return media[0].get("value").replace("\n", "")
+    else:
+        return None
+
+
+def format_mileage(x):
+    x = str(x)
+    return "{},{}".format(x[:-3],  x[-3:])
+
+
+def format_price(x):
+    x = str(x)
+    return "€{},{}".format(x[:-3], x[-3:])
+
+
+def format_location(x):
+    return x.lower().capitalize()
 
 
 def callback_timer(context):
-    context.bot.send_message(chat_id=conf.CHAT_ID, text='One message every minute')
+    response = call_api()
+    results = get_latest(response.get("results"))
+    if results is not None:
+        context.bot.send_message(chat_id=conf.CHAT_ID, text="Nieuwe waggies! Zie hier de {} nieuwste:".format(len(results)), parse_mode="MARKDOWN", disable_web_page_preview=True)
+        for doc in results:
+            city = format_location(doc.get("company").get("city"))
+            province = format_location(doc.get("company").get("province"))
+            price = format_price(doc.get("vehicle").get("askingPrice"))
+            mileage = format_mileage(doc.get("vehicle").get("mileageInKm"))
+            timestamp_posted = (datetime.fromtimestamp(doc.get("meta").get("createdAt") / 1000.0)).strftime("%d %b %Y, %H:%M")
+            id = doc.get("id")
+            lat = doc.get("company").get("coordinates").get("lat")
+            lon = doc.get("company").get("coordinates").get("lon")
+            location = "https://maps.google.com/?q={},{}".format(lat, lon)
+            url = "https://www.anwb.nl/auto/kopen/detail/merk=renault/model=clio/overzicht/{}".format(id)
+            img_url = get_cover_image(doc)
+
+            msg = "Km: {}\nPrijs: {}\nPlaats: {} ({})\nGeplaatst op: {}\nLocatie dealer: [Google Maps]({})\nMeer info: [ANWB]({})".format(
+                mileage, price, city, province, timestamp_posted, location, url
+            )
+            if img_url is not None:
+                context.bot.send_photo(chat_id=conf.CHAT_ID, photo=img_url)
+
+            context.bot.send_message(chat_id=conf.CHAT_ID, text=msg, parse_mode="MARKDOWN", disable_web_page_preview=True)
+    else:
+        context.bot.send_message(chat_id=conf.CHAT_ID, text="Geen nieuwe waggies :(", disable_web_page_preview=True)
 
 
-updater = Updater(conf.TOKEN)
-job_queue = updater.job_queue
+def main():
+    # create updater
+    updater = Updater(conf.TOKEN)
+    job_queue = updater.job_queue
 
-job_minute = job_queue.run_repeating(callback_timer, interval=5, first=0)
-updater.start_polling()
+    # create job and start program
+    job_queue.run_repeating(callback_timer, interval=conf.HOURS*60, first=0)
+    updater.start_polling()
+
+
+if __name__ == "__main__":
+    main()
